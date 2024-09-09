37fbdaef2383c96b8633563b936bc3c71d5899cc
thaison1939 <70836333+thaison1939@users.noreply.github.com>
2024-09-09 18:09:07 +0700

    Update ActivityMain.kt

--- GrabFoodApp/app/src/main/java/com/app/java/grabfoodappproject/MainActivity.java
+++ GrabFoodApp/app/src/main/java/com/app/java/grabfoodappproject/MainActivity.java
@@ -25,37 +25,5 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        /*
-        private void createData() {
-            ArrayList<Shop> shopList = new ArrayList<Shop>();
-
-            Shop shop1 = new Shop(R.drawable.rec_banhmi,"Bánh Mì Việt", "20% off", "Ad");
-            shopList.add(shop1);
-
-            Shop shop2 = new Shop(R.drawable.rec_xoi, "Xôi Gà", "9.000đ off · 50% off", "35 mins · 1.7 km");
-            shopList.add(shop2);
-
-            Shop shop3 = new Shop(R.drawable.rec_pizza, "Pizza 4P", "18.000đ · 80% off", "20 mins · 5 km");
-            shopList.add(shop3);
-
-            Shop shop4 = new Shop(R.drawable.rec_chao, "Cháo Sườn", "18.000đ · 20% off", "1 hr · 20 km");
-            shopList.add(shop4);
-
-            Shop shop5 = new Shop(R.drawable.rec_caphe, "Cà Phê Muối", "15.000đ · 10% off", "10 mins · 3 km");
-            shopList.add(shop5);
-
-            Shop shop6 = new Shop(R.drawable.rec_bundau,"Bún Đậu Mắm Tôm", "30.000đ", "40 mins · 13 km");
-            shopList.add(shop6);
-
-        }
-
-        private void initRecyclerView() {
-            LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-            RecyclerView rv = findViewById(R.id.recycleView);
-            rv.setLayoutManager(layoutManager);
-            Adapter adapter = new Adapter(shoplist);
-            initRecyclerView.setAdapter(adapter);
-        }
-        */
     }
 }
