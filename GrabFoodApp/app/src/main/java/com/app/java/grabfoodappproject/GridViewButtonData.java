556b9617101a2cba1571d361bea1a4b96434bbd5
thaison1939 <70836333+thaison1939@users.noreply.github.com>
2024-09-09 13:55:45 +0700

    HorizontalMenu and Buttons

---
+++ GrabFoodApp/app/src/main/java/com/app/java/grabfoodappproject/GridViewButtonData.java
@@ -0,0 +1,25 @@
+/*
+package com.app.java.grabfoodappproject;
+
+import java.util.ArrayList;
+
+public class GridViewButtonData {
+    public static ArrayList<GridViewButton> getButtonData() {
+        ArrayList<GridViewButton> buttonList = new ArrayList<GridViewButton>();
+
+        GridViewButton button1 = new GridViewButton("Meal under 40K", "Save every day");
+        buttonList.add(button1);
+
+        GridViewButton button2 = new GridViewButton("Quán ngon ĐB", "Ngon phải thử");
+        buttonList.add(button2);
+
+        GridViewButton button3 = new GridViewButton("Siêu tiệc 99K", "Deal to tiệc to");
+        buttonList.add(button3);
+
+        GridViewButton button4 = new GridViewButton("Tiệc tựu trường", "Student back to school");
+        buttonList.add(button4);
+
+        return buttonList;
+    }
+}
+*/
