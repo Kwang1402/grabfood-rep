00af8ef5b05e57746ee8e742ca156f995e0d5544
Kwang1402 <hongquang2k4@gmail.com>
2024-09-14 01:49:30 +0700

    add Food Orders and Dine Out Voucher activity

--- GrabFoodApp/app/src/main/java/com/app/java/grabfoodappproject/fragment/HeaderFragment.java
+++ GrabFoodApp/app/src/main/java/com/app/java/grabfoodappproject/fragment/HeaderFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.app.java.grabfoodappproject.R;
+import com.app.java.grabfoodappproject.activity.FoodOrdersActivity;
 import com.app.java.grabfoodappproject.activity.RestaurantActivity;
 
 public class HeaderFragment extends Fragment {
@@ -40,10 +41,15 @@ public class HeaderFragment extends Fragment {
         spinnerLocation.setAdapter(locationAdapter);
 
         ImageView imageFavourite = view.findViewById(R.id.ic_favourite);
-       // ImageView imageOrder = view.findViewById(R.id.ic_order);
+        ImageView imageOrders = view.findViewById(R.id.ic_order);
 
         imageFavourite.setOnClickListener(v -> {
             Intent intent = new Intent(requireContext(), RestaurantActivity.class);
+            startActivity(intent);
+        });
+
+        imageOrders.setOnClickListener(v -> {
+            Intent intent = new Intent(requireContext(), FoodOrdersActivity.class);
             startActivity(intent);
         });
     }
